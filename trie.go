d5a735e1b937ea00d485566509b087322e15ce97
Benjamin Sladewski <bsladewski@gmail.com>
2019-09-23 20:18:43 -0400

    Implemented the Trie data structure.
    Decided the Trie was not a good fit for the Collection interface.
    Reworked the functions exported by the Trie type.
    Implemented the Trie functions, now it just needs unit tests.

--- trie.go
+++ trie.go
@@ -2,42 +2,114 @@ package gollections
 
 // A Trie is used to quickly check for and retrieve strings.
 type Trie struct {
-	value    byte
-	children map[string]*Trie
-	length   int
+	value    rune
+	children map[rune]*Trie
 }
 
-// Add appends new elements to the collection.
-func (trie *Trie) Add(values ...interface{}) {
-
+// adds the supplied string to the trie character by character.
+func (trie *Trie) add(value []rune, index int) {
+	if index >= len(value) {
+		trie.children[0] = nil
+		return
+	}
+	current := value[index]
+	node, ok := trie.children[current]
+	if !ok {
+		node = &Trie{value: current, children: map[rune]*Trie{}}
+		trie.children[current] = node
+	}
+	node.add(value, index+1)
 }
 
-// Clear removes all elements from the collection.
-func (trie *Trie) Clear() {
-
+// Add inserts new values into the trie.
+func (trie *Trie) Add(values ...string) {
+	if trie.children == nil {
+		trie.children = map[rune]*Trie{}
+	}
+	for _, value := range values {
+		trie.add([]rune(value), 0)
+	}
 }
 
-// Contains checks if the collection contains all specified values.
-func (trie *Trie) Contains(values ...interface{}) bool {
-	return false
+// get finds a node in the trie using the supplied value as a path.
+// Returns nil if no such node is found.
+func (trie *Trie) get(value []rune, index int) *Trie {
+	if index == len(value) {
+		return trie
+	}
+	node, ok := trie.children[value[index]]
+	if !ok {
+		return nil
+	}
+	return node.get(value, index+1)
 }
 
-// IsEmpty checks if the collection contains no elements.
-func (trie *Trie) IsEmpty() bool {
-	return false
+// traverse returns all strings that begin with the specified prefix.
+// If no relevant strings exist, the resulting array will be empty.
+func (trie *Trie) traverse(prefix string) []string {
+	if trie.value == 0 {
+		return []string{prefix}
+	}
+	values := []string{}
+	for _, v := range trie.children {
+		values = append(values, v.traverse(prefix+string(trie.value))...)
+	}
+	return values
 }
 
-// Remove removes all specified values from the collection.
-func (trie *Trie) Remove(values ...interface{}) {
-
+// Complete returns all strings that complete the supplied string.
+// If no relevant strings exist, the resulting array will be empty.
+func (trie *Trie) Complete(value string) []string {
+	node := trie.get([]rune(value), 0)
+	if node == nil {
+		return []string{}
+	}
+	return node.traverse(value)
 }
 
-// Size gets the number of elements in the collection.
-func (trie *Trie) Size() int {
-	return 0
+// contains checks if the trie contains the specified value.
+func (trie *Trie) contains(value []rune, index int) bool {
+	if index == len(value) {
+		_, ok := trie.children[0]
+		return ok
+	}
+	node, ok := trie.children[value[index]]
+	if !ok {
+		return false
+	}
+	return node.contains(value, index+1)
 }
 
-// ToArray gets an array representation of the collection.
-func (trie *Trie) ToArray() []interface{} {
-	return nil
+// Contains checks if the trie contains all specified values.
+func (trie *Trie) Contains(values ...string) bool {
+	for _, value := range values {
+		if !trie.contains([]rune(value), 0) {
+			return false
+		}
+	}
+	return true
+}
+
+// remove deletes the specified value from the trie.
+// Returns true if the node should be removed from its parent.
+func (trie *Trie) remove(value []rune, index int) bool {
+	if index == len(value) {
+		delete(trie.children, 0)
+		return len(trie.children) == 0
+	}
+	node, ok := trie.children[value[index]]
+	if !ok {
+		return false
+	}
+	if node.remove(value, index+1) {
+		delete(trie.children, value[index])
+	}
+	return len(trie.children) == 0
+}
+
+// Remove deletes the specified values from the trie.
+func (trie *Trie) Remove(values ...string) {
+	for _, value := range values {
+		trie.remove([]rune(value), 0)
+	}
 }
